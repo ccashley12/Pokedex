f43c77893b5d579904144a9e2663b004fdbc17c3
ccashley12 <ccashley12@gmail.com>
2024-01-12 13:01:59 -0500

    added pokemon array and obejcts to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello World');
-
-let myfavoriteFood = 'Italian';
-document.write(myfavoriteFood);
+let pokemonList = [
+    {
+        name: 'Charmeleon',
+        type: 'FIRE',
+        height: 1.1,
+        abilities: ['Blaze', 'Solar-power']
+    },
+    {
+        name: 'Butterfree',
+        type: 'FLYING',
+        height: 1.1,
+        abilities: ['Compoundeyes', 'Tinted-lens']
+    },
+    {
+        name: 'Arbok',
+        type: 'POISON',
+        height: 3.5,
+        abilities: ['Intimidte', 'Shed Skin', 'Unnerve']
+    },
+  ]
