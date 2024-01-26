7023293794f229b93effd2b2710cddbd4bfb5ea9
ccashley12 <ccashley12@gmail.com>
2024-01-26 08:59:32 -0500

    updated properties at forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -55,7 +55,7 @@ let pokemonRepository = (function () {
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Pikachu'});
 
-pokemonRepository.getAll().forEach( (item, i, arr) => {
-    document.write(pokemonRepository + ' <br>');
-    ``
+pokemonRepository.getAll().pokemonList.forEach( (item, i, arr) => {
+    document.write(pokemonList[i].name + ' <br>');
+
   })();
