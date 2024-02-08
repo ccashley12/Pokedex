9c6445ce6476436fd598827039ead33880281695
ccashley12 <ccashley12@gmail.com>
2024-02-08 18:36:03 -0500

    deleted code that was not needed

--- js/scripts.js
+++ js/scripts.js
@@ -86,7 +86,7 @@ let pokemonRepository = (function () {
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
 
-        button.addEventListener('click', function (event) {
+        button.addEventListener('click', function () {
             showDetails(pokemon);
         });
     }
