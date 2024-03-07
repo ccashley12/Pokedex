f7261b720135ee27ab9f7b2525067ad3bd0e86c7
ccashley12 <ccashley12@gmail.com>
2024-03-07 13:05:04 -0500

    uncommented necessary code

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ let pokemonRepository = (function () {
         let modalTitle = $(".modal-title");
 
         // clear existing content of the modal
-        // modalBody.empty();
-        // modalTitle.empty();
+        modalBody.empty();
+        modalTitle.empty();
 
         // creating element for name in modal content
         let nameElement = $("<h1>" + item.name + "</h1>");
